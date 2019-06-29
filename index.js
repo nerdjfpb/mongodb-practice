a2abd28acb3f3f77fff71e760698cf33a59a2325
Muhammad Ali <nerdjfpb@gmail.com>
2019-06-29 21:30:14 +0600

    scheme define

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 	.then( () => console.log('Connected to MongoDB...'))
 	.catch(err => console.error('Could not connect to MongoDB ', err)); 
+// String
+// Number
+// Date
+// Buffer
+// Boolean
+// ObjectID
+// Array
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [ String ],
+	date: { type: Date, default: Date.now },
+	isPublished: Boolean
+});
